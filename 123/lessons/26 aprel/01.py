bd5ef9efc9c829035145daa1a1f594c5dad781c8
H1GF <47325507+H1GF@users.noreply.github.com>
2023-05-02 10:18:54 +0700

    Add files via upload

--- 123/lessons/26 aprel/01.py
+++ 123/lessons/26 aprel/01.py
@@ -50,17 +50,17 @@
 
 import os
 
-_path__to_data = '26 aprel/data/'
+path_to_data = '26 aprel/data/'
 
-files = sorted(os.listdir(_path__to_data))
+files = sorted(os.listdir(path_to_data))
 
 for file in files:
-    with open(_path__to_data+file) as f:
+    with open(path_to_data+file) as f:
+        count_a = 0
+        count_e = 0
+        count_k = 0
         for line in f:
-            text = str(line.strip() )
-            count_a = 0
-            count_e = 0
-            count_k = 0
+            line = line.strip()
             for letter in line:
                 if letter == "а" or letter == "А":
                     count_a += 1
@@ -68,7 +68,7 @@ for file in files:
                     count_e += 1
                 if letter == "к" or letter == "К":
                     count_k += 1
-    print(f'В {_path__to_data+file} файле буква "а" встречается {count_a} раз')
-    print(f'В {_path__to_data+file} "e" встречается {count_e} раз')
-    print(f'В {_path__to_data+file} "к" встречается {count_k} раз \n')
+    print(f'В {file} "e" встречается {count_e} раз')
+    print(f'В {file} файле буква "а" встречается {count_a} раз')
+    print(f'В {file} "к" встречается {count_k} раз \n')
 
